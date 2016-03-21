a44dbd6a68dbb4a2c601fc5841f62d379865e9d4
bklynate <nathanielcfa@gmail.com>
2016-03-21 18:35:23 -0400

    add index.html.erb  backbonejs, marionettejs, and underscorejs modified application_controller.rb, routes.rb, application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require lib/underscore
+//= require lib/backbone
+//= require lib/marionette
+//= require backbone/app
 //= require turbolinks
 //= require_tree .
